6208af2a681e20118bd3906b8697c78bafc85ee0
Andrew Nottoli <nottolivc@gmail.com>
2019-06-26 18:28:29 -0700

    MVP + stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -8,23 +8,21 @@ myFunction();
 let anotherFunction = (param) => {
 return param;
 };
-anotherFunction("Example");
+console.log(anotherFunction("Example"));
 
 let add = (param1, param2) => {
   return param1 + param2;
 };
-add(1,2);
+console.log(add(1,2));
 
-let subtract = (param1, param2) => {
-  return param1 - param2;
+let subtract = (x, y) => {
+  return x - y;
 };
-subtract(1,2);
+console.log(subtract(4, 2));
 
 
 //Stretch
 
-exampleArray = [1,2,3,4];
-const triple = exampleArray.map((num) => {
-  return num * 3;
-});
+const exampleArray = [1,2,3,4];
+const triple = exampleArray.map((num =>  num * 3);
 console.log(triple);
